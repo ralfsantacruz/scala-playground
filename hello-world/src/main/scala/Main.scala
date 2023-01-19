fb0609d41b2b3e077d99d356d03030c0641b4b80
Rafael Santa Cruz <rafaelsantacruz@rafaels-mbp.lan>
2023-01-18 16:07:00 -0800

    add common.yml

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,5 +1,17 @@
-@main def hello: Unit = 
+@main def happyBirthday(age: Int, name: String) = 
+
+  val suffix = (age % 100) match
+    case 11 | 12 | 13 => "th"
+    case _ => (age % 10) match
+      case 1 => "st"
+      case 2 => "nd"
+      case 3 => "rd"
+      case _ => "th"
+  
+  val stringBuilder = StringBuilder(s"Happy $age$suffix birthday $name!")
+
   println("Hello world!")
+  println(stringBuilder.toString)
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
