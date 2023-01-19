5a322b225601f9f5085cd9fde15422b1f1b1259a
Rafael Santa Cruz <rafaelsantacruz@rafaels-mbp.lan>
2023-01-18 23:49:15 -0800

    add more messing around code

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,17 +1,63 @@
-@main def happyBirthday(age: Int, name: String) = 
+object hello {
+  def main(args: Array[String]) = {
+    println("Hello, World!")
+    println("-" * 20)
+    valuePractice("placeholder")
+    println("-" * 20)
+    stringInterpolation("John", "Doe")
+    println("-" * 20)
+    tripleQuotes()
+    println("-" * 20)
+    controlStructures.main()
+  }
 
-  val suffix = (age % 100) match
-    case 11 | 12 | 13 => "th"
-    case _ => (age % 10) match
-      case 1 => "st"
-      case 2 => "nd"
-      case 3 => "rd"
-      case _ => "th"
-  
-  val stringBuilder = StringBuilder(s"Happy $age$suffix birthday $name!")
+  def valuePractice(args: String) = {
+    val a = 0 // this is immutable
+    var b = 1 // this is mutable
 
-  println("Hello world!")
-  println(stringBuilder.toString)
-  println(msg)
+    println(b) // should output 1
 
-def msg = "I was compiled by Scala 3. :)"
+    b = 2
+
+    println(b) // should output 2
+  }
+
+  def stringInterpolation(firstName: String, lastName: String) = {
+    println(s"Name: $firstName $lastName")
+  }
+
+  def tripleQuotes() = {
+    val quote = """
+      This is a multiline string
+      using triple quotes
+    """
+    println(quote)
+  }
+
+}
+
+object controlStructures {
+  def main() = {
+    ifElse()
+    forLoops()
+    println("-" * 20)
+    guardedForLoops()
+  }
+
+  def ifElse() = {
+    val x = if (1 > 0) 69 else 1
+    val testPasses = x == 69
+    assert(testPasses)
+  }
+
+  def forLoops() = {
+    val ints = List(1, 2, 3, 4, 5)
+    for (i <- ints)
+      println(i)
+  }
+
+  def guardedForLoops() = {
+    for (i <- 0 to 9 if i % 3 == 0)
+      println(i) // print factors of 3 only
+  }
+}
